5ca8d1281a07f0c2df7f6d8434449a5b9c8cc4b5
krava <kravad2002@gmail.com>
2023-03-07 08:12:59 +0300

    connect BD

--- src/main/java/com/bot/service/DataConnection.java
+++ src/main/java/com/bot/service/DataConnection.java
@@ -6,8 +6,11 @@ import java.sql.*;
 
 @Slf4j
 public class DataConnection {
+    private static Connection connection = null;
+    private static Statement statement = null;
+
+
     public static void bdConnection(){
-        Connection connection = null;
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:/media/krava/Новый том/Telegram_friend/src/main/resources/list.sqbpro");
@@ -15,5 +18,16 @@ public class DataConnection {
         }catch (Exception e){
             log.error("Error occurred: " + e.getMessage());
         }
+    }
+    public static void getValues() throws SQLException {
+        try {
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM COMPANY;");
+            log.info("connect successful");
+        }catch (Exception e){
+            log.error("Error occurred: " + e.getMessage());
+        }
+
+
     }
 }
